3bd51e8524ff3c9a28ee9ee4d60098959f32c5a5
pellinuz <pellindiego485@gmail.com>
2025-05-29 20:26:34 +0200

    V 2k25

--- sw.js
+++ sw.js
@@ -1,37 +1,38 @@
 const staticCacheName = 'site-static-v1';
 const assets = [
-  '/',
-  'https://grest.pellinuz.repl.co/bans-list',
+	'/',
 	"/manifest.json",
 	"/pwa.js",
-  '/script.js',
+	'/script.js',
 	'/style.css'
 ];
+
 // install event
 self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
+	console.log('service worker installed');
 });
+
 // activate event
 self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
+	evt.waitUntil(
+		caches.keys().then(keys => {
+			return Promise.all(keys.map(key => caches.delete(key)));
+		})
+	);
 });
+
 // fetch event
 self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
+	evt.respondWith(
+		fetch(evt.request).then(networkRes => {
+			// aggiorna anche la cache in background se vuoi
+			return caches.open(staticCacheName).then(cache => {
+				cache.put(evt.request, networkRes.clone());
+				return networkRes;
+			});
+		}).catch(() => {
+			// se fallisce la fetch (offline), usa la cache
+			return caches.match(evt.request);
+		})
+	);
 });
